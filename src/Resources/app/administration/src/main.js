14e2cfd41aa4cfce4d3dd3c8fc43bdde56c64fe9
rodion_medvedev <rmedvedev@insys.io>
2022-06-16 13:37:21 +0600

    add admministration app elements

---
+++ src/Resources/app/administration/src/main.js
@@ -0,0 +1,4 @@
+import './module/sw-cms/blocks/sidebar/mm-car-catalog';
+import './module/sw-cms/blocks/sidebar/mm-car-search';
+import './module/sw-cms/elements/mm-car-catalog';
+import './module/sw-cms/elements/mm-car-search';
